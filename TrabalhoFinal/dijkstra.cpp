d4e887a8c9af0cc10ccb5419f1e02bedab75d064
HandreMelo <34012482+HandreMelo@users.noreply.github.com>
2019-09-05 17:05:42 -0300

    Rename dijkstra.cpp to TrabalhoFinal/dijkstra.cpp

--- TrabalhoFinal/dijkstra.cpp
+++ TrabalhoFinal/dijkstra.cpp
@@ -1,4 +1,4 @@
-// ImplementaÁ„o do algoritmo de Dijkstra
+// Implementa√ß√£o do algoritmo de Dijkstra
 // Teste: http://br.spoj.com/problems/ENGARRAF/
 
 #include <iostream>
@@ -11,9 +11,9 @@ using namespace std;
 class Grafo
 {
 private:
-	int V; // n˙mero de vÈrtices
+	int V; // n√∫mero de v√©rtices
 
-	// ponteiro para um array contendo as listas de adjacÍncias
+	// ponteiro para um array contendo as listas de adjac√™ncias
 	list<pair<int, int> > * adj;
 
 public:
@@ -21,16 +21,16 @@ public:
 	// construtor
 	Grafo(int V)
 	{
-		this->V = V; // atribui o n˙mero de vÈrtices
+		this->V = V; // atribui o n√∫mero de v√©rtices
 
 		/*
-			cria as listas onde cada lista È uma lista de pairs
-			onde cada pair È formado pelo vÈrtice destino e o custo
+			cria as listas onde cada lista √© uma lista de pairs
+			onde cada pair √© formado pelo v√©rtice destino e o custo
 		*/
 		adj = new list<pair<int, int> >[V];
 	}
 
-	// adiciona uma aresta ao grafo de v1 ‡ v2
+	// adiciona uma aresta ao grafo de v1 √† v2
 	void addAresta(int v1, int v2, int custo)
 	{
 		adj[v1].push_back(make_pair(v2, custo));
@@ -39,27 +39,27 @@ public:
 	// algoritmo de Dijkstra
 	int dijkstra(int orig, int dest)
 	{
-		// vetor de dist‚ncias
+		// vetor de dist√¢ncias
 		int dist[V];
 
 		/*
-		   vetor de visitados serve para caso o vÈrtice j· tenha sido
-		   expandido (visitado), n„o expandir mais
+		   vetor de visitados serve para caso o v√©rtice j√° tenha sido
+		   expandido (visitado), n√£o expandir mais
 		*/
 		int visitados[V];
 
-		// fila de prioridades de pair (distancia, vÈrtice)
+		// fila de prioridades de pair (distancia, v√©rtice)
 		priority_queue < pair<int, int>,
 					   vector<pair<int, int> >, greater<pair<int, int> > > pq;
 
-		// inicia o vetor de dist‚ncias e visitados
+		// inicia o vetor de dist√¢ncias e visitados
 		for(int i = 0; i < V; i++)
 		{
 			dist[i] = INFINITO;
 			visitados[i] = false;
 		}
 
-		// a dist‚ncia de orig para orig È 0
+		// a dist√¢ncia de orig para orig √© 0
 		dist[orig] = 0;
 
 		// insere na fila
@@ -69,10 +69,10 @@ public:
 		while(!pq.empty())
 		{
 			pair<int, int> p = pq.top(); // extrai o pair do topo
-			int u = p.second; // obtÈm o vÈrtice do pair
+			int u = p.second; // obt√©m o v√©rtice do pair
 			pq.pop(); // remove da fila
 
-			// verifica se o vÈrtice n„o foi expandido
+			// verifica se o v√©rtice n√£o foi expandido
 			if(visitados[u] == false)
 			{
 				// marca como visitado
@@ -80,17 +80,17 @@ public:
 
 				list<pair<int, int> >::iterator it;
 
-				// percorre os vÈrtices "v" adjacentes de "u"
+				// percorre os v√©rtices "v" adjacentes de "u"
 				for(it = adj[u].begin(); it != adj[u].end(); it++)
 				{
-					// obtÈm o vÈrtice adjacente e o custo da aresta
+					// obt√©m o v√©rtice adjacente e o custo da aresta
 					int v = it->first;
 					int custo_aresta = it->second;
 
 					// relaxamento (u, v)
 					if(dist[v] > (dist[u] + custo_aresta))
 					{
-						// atualiza a dist‚ncia de "v" e insere na fila
+						// atualiza a dist√¢ncia de "v" e insere na fila
 						dist[v] = dist[u] + custo_aresta;
 						pq.push(make_pair(dist[v], v));
 					}
@@ -98,7 +98,7 @@ public:
 			}
 		}
 
-		// retorna a dist‚ncia mÌnima atÈ o destino
+		// retorna a dist√¢ncia m√≠nima at√© o destino
 		return dist[dest];
 	}
 };
