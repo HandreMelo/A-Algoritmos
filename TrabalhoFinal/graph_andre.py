555a30d8a2a0d6bacc349f710fd652313532dc8c
PhelipeH <phelipe.hass@gmail.com>
2019-10-22 12:55:48 -0300

    Corrigido tratativa da aresta com valor negativo

--- TrabalhoFinal/graph_andre.py
+++ TrabalhoFinal/graph_andre.py
@@ -22,13 +22,12 @@ def ler_arquivo():
 
         for i in range(2, n + 2):
             pesos.append(vetor_ler[i].split(','))
-
+            
+        # TRATA ARESTA COM VALOR NEGATIVO    
         for i in range(0, len(pesos)):
             for x in range(0, len(pesos[i])):
-                pesoAtual = int(pesos[i][x])
-                if pesoAtual < 0:
-                    pesoAtual *= -1
-                    pesos[i][x] = str(pesoAtual)
+                if int(pesos[i][x]) < 0:
+                    pesos[i][x] = str(0)
 
         for i in range(0, e):
             ler_entregas.append(vetor_ler[n + 3 + i].split(','))
